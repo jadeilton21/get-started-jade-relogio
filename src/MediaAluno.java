9d6677ca1725523042a9ca30cc5c6291fbb3c3ea
jadeilton <jadeiltondm@gmail.com>
2025-04-05 15:34:12 -0300

    feat

---
+++ src/MediaAluno.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MediaAluno {
+
+
+
+
+    public static void main(String[] args) {
+
+
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        ArrayList<Integer> notas = new ArrayList<>();
+
+        while (true) {
+            double nota = scanner.nextDouble();
+            if (nota <= 0) break;
+
+        }
+    }
+}
