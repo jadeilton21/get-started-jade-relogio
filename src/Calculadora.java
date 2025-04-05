c77234f1ec1fee742c977d9c866420001aacf03d
jadeilton <jadeiltondm@gmail.com>
2025-04-04 22:43:18 -0300

    feat: projeto bootcamp

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,31 +1,43 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Calculadora {
+
+
     public static void main(String[] args) {
+
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Digite o primeiro número: ");
+
+        System.out.println("Digite um numero quantos numeros quiser: ");
         double num1 = scanner.nextDouble();
 
-        System.out.print("Digite a operação (+, -, *, /): ");
+
+        System.out.println("Digite a operação: (+, -, *, /) ");
         char operacao = scanner.next().charAt(0);
 
-        System.out.print("Digite o segundo número: ");
+
+        System.out.println("Digite o 2 numero: ");
         double num2 = scanner.nextDouble();
 
         double resultado = 0;
 
-        switch (operacao) {
+
+        switch(operacao){
+
             case '+': resultado = num1 + num2; break;
             case '-': resultado = num1 - num2; break;
             case '*': resultado = num1 * num2; break;
-            case '/': resultado = (num2 != 0) ? num1 / num2 : Double.NaN; break;
-            default: System.out.println("Operação inválida");
+            case '/': resultado = num1 / num2; break;
+            default:
+                System.out.println("Operação Invalida");
         }
 
+
         System.out.println("Resultado: " + resultado);
+
         scanner.close();
+
+
     }
-    }
+}
